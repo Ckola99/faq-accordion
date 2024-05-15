e44c78795538e70fdeb0a3a5ad93c64dcca7782a
Ckola99 <Christopherkola@gmail.com>
2024-05-15 16:36:30 +0200

    faq-accordion

--- main.js
+++ main.js
@@ -3,27 +3,43 @@ const questionHeaders = document.querySelectorAll('.question-box-header');
 
 
 
-questionHeaders.forEach(header => {
-	// Check if the answer is initially displayed and show the minus button accordingly
+questionHeaders.forEach((header, index) => {
+	const answer = header.nextElementSibling;
 
-	header.addEventListener('click', () => {
-		const answer = header.nextElementSibling;
-
-		// Toggle the display property of the answer
+	// Function to toggle answer visibility
+	const toggleAnswer = () => {
 		if (answer.style.display === 'block') {
 			answer.style.display = 'none';
-
-			// Change plus and minus button visibility
 			header.querySelector('.plus').classList.remove('hidden');
-			header.querySelector('.minus').style.display = 'none'
-
+			header.querySelector('.minus').classList.add('hidden');
 		} else {
 			answer.style.display = 'block';
-
-			// Change plus and minus button visibility
 			header.querySelector('.plus').classList.add('hidden');
-			header.querySelector('.minus').style.display = 'block'
+
+			
+		}
+	};
+
+	// Click event listener for mouse interaction
+	header.addEventListener('click', toggleAnswer);
+
+	// Keyboard event listener for navigating and toggling answers
+	header.addEventListener('keydown', event => {
+		if (event.key === 'Enter' || event.key === ' ') {
+			// Toggle answer visibility
+			toggleAnswer();
+		} else if (event.key === 'ArrowUp') {
+			// Move to previous question
+			const prevIndex = index === 0 ? questionHeaders.length - 1 : index - 1;
+			questionHeaders[prevIndex].focus();
+		} else if (event.key === 'ArrowDown') {
+			// Move to next question
+			const nextIndex = index === questionHeaders.length - 1 ? 0 : index + 1;
+			questionHeaders[nextIndex].focus();
 		}
 	});
+
+	// Add tabindex attribute to make question headers focusable
+	header.setAttribute('tabindex', '0');
 
 });
